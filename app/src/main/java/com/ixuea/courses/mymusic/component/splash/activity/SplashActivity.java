d1cd905ba8d1ab73f5d0468bdb4d79c6582d3b8c
1036200167@qq.com <1036200167@qq.com>
2022-07-08 10:24:15 +0800

    7245 Add splash layout

---
+++ app/src/main/java/com/ixuea/courses/mymusic/component/splash/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.ixuea.courses.mymusic.component.splash.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ixuea.courses.mymusic.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
