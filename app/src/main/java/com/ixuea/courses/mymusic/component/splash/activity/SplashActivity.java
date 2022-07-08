8975ffdcb3b242511ad7c580608e83df39debd2e
1036200167@qq.com <1036200167@qq.com>
2022-07-08 10:56:39 +0800

    7246 Remove action bar and show year

--- app/src/main/java/com/ixuea/courses/mymusic/component/splash/activity/SplashActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/component/splash/activity/SplashActivity.java
@@ -1,10 +1,13 @@
 package com.ixuea.courses.mymusic.component.splash.activity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ixuea.courses.mymusic.R;
+import com.ixuea.courses.mymusic.util.SuperDateUtil;
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,5 +15,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //设置沉浸式状态栏
+        QMUIStatusBarHelper.translucent(this);
+
+        //状态栏文字黑色
+        QMUIStatusBarHelper.setStatusBarLightMode(this);
+
+        //设置版本年份
+        int year = SuperDateUtil.currentYear();
+
+        TextView copyroghtView = findViewById(R.id.copyright);
+        copyroghtView.setText(getResources().getString(R.string.copyright, year));
     }
 }
