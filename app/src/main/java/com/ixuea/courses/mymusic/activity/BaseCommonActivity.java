f34b40c1318186870ea13717e9abb4b3763cad4f
1036200167@qq.com <1036200167@qq.com>
2022-07-08 14:14:22 +0800

    7256 启动界面相关功能、封装常用通用控制器

---
+++ app/src/main/java/com/ixuea/courses/mymusic/activity/BaseCommonActivity.java
@@ -0,0 +1,7 @@
+package com.ixuea.courses.mymusic.activity;
+
+/**
+ * 通用界面逻辑
+ */
+public class BaseCommonActivity extends BaseActivity {
+}
--- app/src/main/java/com/ixuea/courses/mymusic/component/splash/activity/SplashActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/component/splash/activity/SplashActivity.java
@@ -3,29 +3,47 @@ package com.ixuea.courses.mymusic.component.splash.activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.ixuea.courses.mymusic.R;
+import com.ixuea.courses.mymusic.activity.BaseLogicActivity;
+import com.ixuea.courses.mymusic.util.SuperDarkUtil;
 import com.ixuea.courses.mymusic.util.SuperDateUtil;
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
 
-public class SplashActivity extends AppCompatActivity {
+/**
+ * 启动界面
+ */
+public class SplashActivity extends BaseLogicActivity {
+
+    private TextView copyroghtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        copyroghtView = findViewById(R.id.copyright);
         //设置沉浸式状态栏
         QMUIStatusBarHelper.translucent(this);
 
-        //状态栏文字黑色
-        QMUIStatusBarHelper.setStatusBarLightMode(this);
+        if (SuperDarkUtil.isDark(this)) {
+            //状态栏文字白色
+            QMUIStatusBarHelper.setStatusBarLightMode(this);
+        } else {
+            //状态栏文字黑色
+            QMUIStatusBarHelper.setStatusBarLightMode(this);
+        }
+    }
 
+    @Override
+    protected void initDatum() {
+        super.initDatum();
         //设置版本年份
         int year = SuperDateUtil.currentYear();
-
-        TextView copyroghtView = findViewById(R.id.copyright);
         copyroghtView.setText(getResources().getString(R.string.copyright, year));
     }
 }
